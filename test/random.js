a18081868357bf1fc599fad9221601c00b9f01c7
Andreas Stassivik <andreas@stassi.net>
2018-03-07 00:49:34 -0800

    +tests for #evolve in random module

--- test/random.js
+++ test/random.js
@@ -3,17 +3,25 @@ const { hex16 } = require('../src/random');
 
 describe('hex16', () => {
   const knownSeed = 'known seed';
+  const { evolve, seed } = hex16(knownSeed);
+  const { evolve: evolveGen1, seed: seedGen1 } = evolve();
 
   describe('seed', () => {
     it('should return the constructed seed', () => {
-      const { seed } = hex16(knownSeed);
       expect(seed()).to.equal(knownSeed);
     });
   });
 
   describe('evolve', () => {
-    it('should transform #seed to return a value different than constructed');
-    it('should be chainable');
+    it('should transform #seed to return a value different than constructed', () => {
+      expect(seedGen1()).to.be.a('String').and.not.equal(knownSeed);
+    });
+
+    it('should be chainable', () => {
+      const { seed: seedGen2 } = evolveGen1();
+      expect(seedGen2()).to.be.a('String');
+    });
+
     it('should be deterministic when chained and constructed from a known seed');
   });
 
