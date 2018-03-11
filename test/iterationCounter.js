2aaf80d4af6c6328fae27681767dad4a7525ab16
Andreas Stassivik <andreas@stassi.net>
2018-03-11 03:32:51 -0700

    iterationCounter test formatting

--- test/iterationCounter.js
+++ test/iterationCounter.js
@@ -3,26 +3,35 @@ const { iterationCounter } = require('../src/iterationCounter');
 
 describe('iterationCounter', () => {
   describe('#increment', () => {
+    const { increment } = iterationCounter({ iterations: 0, limit: 5 });
+
     it('should increment iterations +1', () => {
-      const { status } = iterationCounter({ iterations: 0, limit: 5 })
-        .increment();
+      const { status } = increment();
       expect(status()).to.contain({ iterations: 1 });
+    });
+
+    it('should be chainable', () => {
+      const { status } = increment()
+        .increment();
+      expect(status()).to.contain({ iterations: 2 });
     });
   });
 
   describe('#limitReached', () => {
+    const limit = 1;
+
     it('should return true when iterations > limit', () => {
-      const { limitReached } = iterationCounter({ iterations: 2, limit: 1 });
+      const { limitReached } = iterationCounter({ limit, iterations: 2 });
       expect(limitReached()).to.be.true;
     });
 
     it('should return true when iterations == limit', () => {
-      const { limitReached } = iterationCounter({ iterations: 1, limit: 1 });
+      const { limitReached } = iterationCounter({ limit, iterations: 1 });
       expect(limitReached()).to.be.true;
     });
 
     it('should return false when iterations < limit', () => {
-      const { limitReached } = iterationCounter({ iterations: 0, limit: 1 });
+      const { limitReached } = iterationCounter({ limit, iterations: 0 });
       expect(limitReached()).to.be.false;
     });
   });
